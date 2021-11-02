a8fda48ad1534aaeea1d453e9dac734764eb754d
Amina Makhusseyeva <amina.makhusseyeva@gmail.com>
2021-11-02 11:07:34 +0400

    Task management app with 7 CRUD actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # to display details of each task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+    # @task = Task.new(task_params)
+    # if @task.save
+    #   redirect_to task_path(@task)
+    # else
+    #   render :new
+    # end
+  end
+
+  def edit
+    # would edit task
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to @task
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  #   # Destroy a task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+
+  # # Create a new task
+  # post '/tasks', to: 'tasks#create'
+
+  # # Update a task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+  resources :tasks, except: :index
 end
